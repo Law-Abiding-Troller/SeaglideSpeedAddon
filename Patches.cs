867abf6f17f9b775a0138ede98ac7b7f0bfe4dcc
Law Abiding Troller <colton.trash5.5@proton.me>
2025-04-06 11:15:50 -0500

    v0.0.0 Source Code. Still making

---
+++ Patches.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using LawAbidingTroller.SeaglideModConcept;
+using LiteralSeaglideUpgrades;
+
+
+namespace SeaglideSpeedAddon
+{
+    [HarmonyPatch(typeof(LawAbidingTroller.SeaglideModConcept.Plugin))] //patch the plugin class
+    public class PluginPatches
+    {
+        [HarmonyPatch(nameof(LawAbidingTroller.SeaglideModConcept.Plugin.Update))] //patch the update method
+        [HarmonyPostfix]
+        public static void Update_Postfix(LawAbidingTroller.SeaglideModConcept.Plugin __instance)
+        {
+            
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,46 @@
+﻿using System.Reflection;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using Nautilus.Assets;
+using Unity.Collections;
+
+namespace SeaglideSpeedAddon
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    [BepInDependency("com.snmodding.nautilus")]
+    [BepInDependency("com.lawabidingtroller.literalseaglideupgrades")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public new static ManualLogSource Logger { get; private set; }
+
+        private static Assembly Assembly { get; } = Assembly.GetExecutingAssembly();
+
+        private void Awake()
+        {
+            // set project-scoped logger instance
+            Logger = base.Logger;
+
+            // Initialize custom prefabs
+            InitializePrefabs();
+
+            // register harmony patches, if there are any
+            Harmony.CreateAndPatchAll(Assembly, $"{PluginInfo.PLUGIN_GUID}");
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+        public static float[] speedmultiplier = { 8 };
+        public static CustomPrefab[] speedprefab;
+        public static PrefabInfo[] prefabinfo;
+        public static int index;
+        public static float changespeedmultiplier = 4;
+        private void InitializePrefabs()
+        {
+            for (index = 0; index < 10; index++)
+            {
+                speedmultiplier[index] += changespeedmultiplier;
+                changespeedmultiplier++;
+                SeaglideSpeedModulePrefab.Register();
+            }
+        }
+    }
+}

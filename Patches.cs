a6b161ee287e983abe1a97747f14806eda8d48e0
Law Abiding Troller <colton.trash5.5@proton.me>
2025-04-08 20:31:58 -0500

    Broken Source Code.

--- Patches.cs
+++ Patches.cs
@@ -1,9 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using HarmonyLib;
+using LawAbidingTroller.LiteralSeaglideUpgrades;
 using LawAbidingTroller.LiteralSeaglideUpgrades.Seaglide_Modules.Efficiency_Modules;
 using LawAbidingTroller.SeaglideModConcept;
 using LawAbidingTroller.SeaglideModConcept.SeaglideModules.SpeedPrefab;
@@ -21,6 +22,7 @@ namespace SeaglideSpeedAddon
         {
             if (__instance.defineallowedtech && !Plugin.whatdoicallthislol)
             {
+                var allowedtech = new TechType[17] { TechType.SeaTreaderPoop, SeaglideSpeedModulePrefab.Info.TechType, SeaglideSpeedModuleMk2.Info.TechType, SeaglideSpeedModuleMk3.Info.TechType, SeaglideEfficiencyModuleMk1.Info.TechType, SeaglideEfficiencyModuleMk2.Info.TechType, SeaglideEfficiencyModuleMk3.Info.TechType, TechType.None, TechType.None, TechType.None, TechType.None, TechType.None, TechType.None, TechType.None, TechType.None, TechType.None, TechType.None };
                 __instance.allowedtech[7] = Plugin.prefabinfo[0].TechType;
                 __instance.allowedtech[8] = Plugin.prefabinfo[1].TechType;
                 __instance.allowedtech[9] = Plugin.prefabinfo[2].TechType;
@@ -73,83 +75,6 @@ namespace SeaglideSpeedAddon
             if (__instance.seaglidestorage[0].container.Contains(Plugin.prefabinfo[9].TechType))
             {
                 __instance.currentspeed = 14;
-            }
-        }
-        [HarmonyPatch(nameof(LawAbidingTroller.SeaglideModConcept.Plugin.UpdateSeaglideSpeed))] //patch the update method
-        [HarmonyPostfix]
-        public static void UpdateSeaglideSpeed_PostFix(LawAbidingTroller.SeaglideModConcept.Plugin __instance)
-        {
-            switch (__instance.currentspeed)
-            {
-                case 0:
-                    __instance.ResetSeaglideSpeed();
-                    Player.main.UpdateMotorMode();
-                    break;
-                case 1:
-                    __instance.IncreaseSeaglideSpeed(1 / 2f);
-                    Player.main.UpdateMotorMode();
-                    if (__instance.instance.activeState && __instance.instance.timeSinceUse >= 1f)
-                    {
-                        __instance.instance.energyMixin.AddEnergy(0f - 0.1f);
-                    }
-                    break;
-                case 2:
-                    __instance.IncreaseSeaglideSpeed(2.0f);
-                    Player.main.UpdateMotorMode();
-                    break;
-                case 3:
-                    __instance.IncreaseSeaglideSpeed(SeaglideSpeedModuleMk2.mk2speedmultiplier);
-                    Player.main.UpdateMotorMode();
-                    break;
-                case 4:
-                    __instance.IncreaseSeaglideSpeed(SeaglideSpeedModuleMk3.mk3speedmultiplier);
-                    Player.main.UpdateMotorMode();
-                    break;
-                case 5:
-                    __instance.IncreaseSeaglideSpeed(Plugin.speedmultiplier[0]);
-                    Player.main.UpdateMotorMode();
-                    break;
-                case 6:
-                    __instance.IncreaseSeaglideSpeed(Plugin.speedmultiplier[1]);
-                    Player.main.UpdateMotorMode();
-                    break;
-                case 7:
-                    __instance.IncreaseSeaglideSpeed(Plugin.speedmultiplier[2]);
-                    Player.main.UpdateMotorMode();
-                    break;
-                case 8:
-                    __instance.IncreaseSeaglideSpeed(Plugin.speedmultiplier[3]);
-                    Player.main.UpdateMotorMode();
-                    break;
-                case 9:
-                    __instance.IncreaseSeaglideSpeed(Plugin.speedmultiplier[4]);
-                    Player.main.UpdateMotorMode();
-                    break;
-                case 10:
-                    __instance.IncreaseSeaglideSpeed(Plugin.speedmultiplier[5]);
-                    Player.main.UpdateMotorMode();
-                    break;
-                case 11:
-                    __instance.IncreaseSeaglideSpeed(Plugin.speedmultiplier[6]);
-                    Player.main.UpdateMotorMode();
-                    break;
-                case 12:
-                    __instance.IncreaseSeaglideSpeed(Plugin.speedmultiplier[7]);
-                    Player.main.UpdateMotorMode();
-                    break;
-                case 13:
-                    __instance.IncreaseSeaglideSpeed(Plugin.speedmultiplier[8]);
-                    Player.main.UpdateMotorMode();
-                    break;
-                case 14:
-                    __instance.IncreaseSeaglideSpeed(Plugin.speedmultiplier[9]);
-                    Player.main.UpdateMotorMode();
-                    break;
-                default:
-                    LawAbidingTroller.SeaglideModConcept.Plugin.Logger.LogWarning("Unknown Speed Selection. Defaulting to normal");
-                    __instance.ResetSeaglideSpeed();
-                    Player.main.UpdateMotorMode();
-                    break;
             }
         }
     }

c2b8706ab1f79e02664d49ce7d847c787f4a05aa
Law Abiding Troller <colton.trash5.5@proton.me>
2025-04-10 09:45:56 -0500

    Broken Source Code.

---
+++ Patches.cs
@@ -0,0 +1,35 @@
+﻿using HarmonyLib;
+using LawAbidingTroller.LiteralSeaglideUpgrades;
+using LawAbidingTroller.LiteralSeaglideUpgrades.Seaglide_Modules.Efficiency_Modules;
+using LawAbidingTroller.SeaglideModConcept;
+using LawAbidingTroller.SeaglideModConcept.SeaglideModules.SpeedPrefab;
+using LiteralSeaglideUpgrades;
+
+
+namespace SeaglideSpeedAddon
+{
+    [HarmonyPatch(typeof(LawAbidingTroller.SeaglideModConcept.Plugin))] //patch the plugin class
+    public class PluginPatches
+    {
+        [HarmonyPatch(nameof(LawAbidingTroller.SeaglideModConcept.Plugin.Update))] //patch the update method
+        [HarmonyPostfix]
+        public static void Update_Postfix(LawAbidingTroller.SeaglideModConcept.Plugin instance)
+        {
+            if (instance.defineallowedtech && !Plugin.Whatdoicallthislol)
+            {
+                instance.allowedtech[7] = Plugin.Prefabinfo[0].TechType;
+                instance.allowedtech[8] = Plugin.Prefabinfo[1].TechType;
+                instance.allowedtech[9] = Plugin.Prefabinfo[2].TechType;
+                instance.allowedtech[10] = Plugin.Prefabinfo[3].TechType;
+                instance.allowedtech[11] = Plugin.Prefabinfo[4].TechType;
+                instance.allowedtech[12] = Plugin.Prefabinfo[5].TechType;
+                instance.allowedtech[13] = Plugin.Prefabinfo[6].TechType;
+                instance.allowedtech[14] = Plugin.Prefabinfo[7].TechType;
+                instance.allowedtech[15] = Plugin.Prefabinfo[8].TechType;
+                instance.allowedtech[16] = Plugin.Prefabinfo[9].TechType;
+                instance.seaglidestorage[0].container.SetAllowedTechTypes(instance.allowedtech);
+                Plugin.Whatdoicallthislol = true;
+            }
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,55 @@
+﻿using System.Reflection;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using Nautilus.Assets;
+using LawAbidingTroller.SeaglideModConcept.SeaglideModules.SpeedPrefab;
+
+namespace SeaglideSpeedAddon
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    [BepInDependency("com.snmodding.nautilus", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("com.lawabidingtroller.literalseaglideupgrades", BepInDependency.DependencyFlags.HardDependency)]
+
+    public class Plugin : BaseUnityPlugin
+    {
+        public new static ManualLogSource Logger { get; private set; }
+
+        private static Assembly Assembly { get; } = Assembly.GetExecutingAssembly();
+
+        private void Awake()
+        {
+            // set project-scoped logger instance
+            Logger = base.Logger;
+            // Initialize custom prefabs after a slight delay
+            InitializePrefabs();
+
+            // register harmony patches, if there are any
+            Harmony.CreateAndPatchAll(Assembly, $"{PluginInfo.PLUGIN_GUID}");
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+        public static bool Whatdoicallthislol = false;
+        public static float[] Speedmultiplier = { 8, 12, 17, 23, 30, 38, 47, 57, 68, 80 };
+        public static CustomPrefab[] Speedprefab = new CustomPrefab[10];
+        public static PrefabInfo[] Prefabinfo = new PrefabInfo[10];
+        public static int Index;
+        public static float Changespeedmultiplier = 4;
+        private static void InitializePrefabs()
+        {
+            for (Index = 0; Index < 10; Index++)
+            {
+                Speedmultiplier[Index] += Changespeedmultiplier;
+                SeaglideSpeedModulePrefab.Register();
+                Logger.LogInfo(Index.ToString());
+                if (Index == 8)
+                {
+                    Changespeedmultiplier = 20;
+                }
+                else
+                {
+                    Changespeedmultiplier++;
+                }
+            }
+        }
+    }
+}
